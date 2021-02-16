c849b5bc3014fc58e687bef50547fdac93fd6daa
Matthew Caseres <matthewcaseres@outlook.com>
2021-02-15 19:43:31 -0600

    delete some things

--- index.js
+++
@@ -1,9 +0,0 @@
-var { summaryToUrlTree } = require("github-books");
-
-(async () => {
-  let tree = await summaryToUrlTree({
-    url: "https://github.com/MatthewCaseres/mdExperiments/blob/main/tsts",
-  })
-  console.log(tree)
-})()
-
